7b5a66c42cd10de5ad4513eb0bb3b04aa170a12d
pmazer <pmazer@aa934db6-0155-11de-9fdb-3debd5008f28>
2009-04-16 17:58:40 +0000

    added capture state to State class
    git-svn-id: http://asuahsahua.googlecode.com/svn/trunk@36 aa934db6-0155-11de-9fdb-3debd5008f28

--- 3240/workspace/mawk/cs3240/sp09/RegularLanguage/State.java
+++ 3240/workspace/mawk/cs3240/sp09/RegularLanguage/State.java
@@ -3,10 +3,18 @@ package cs3240.sp09.RegularLanguage;
 public class State {
 	int index;
 	boolean isFinal;
+	boolean isCapture;
 	
 	public State(int index, boolean isFinal){
 		this.index = index;
 		this.isFinal = isFinal;
+		this.isCapture = false;
+	}
+	
+	public State(int index, boolean isFinal, boolean isCapture){
+		this.index = index;
+		this.isFinal = isFinal;
+		this.isCapture = isCapture;
 	}
 	
 	public State clone(){
