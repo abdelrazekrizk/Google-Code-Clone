9ba596e0592ae7d5d4c1e623019f1e0aee92f522
asuahsahua <asuahsahua@aa934db6-0155-11de-9fdb-3debd5008f28>
2009-04-22 19:10:18 +0000

    git-svn-id: http://asuahsahua.googlecode.com/svn/trunk@43 aa934db6-0155-11de-9fdb-3debd5008f28

--- 3451/project 5/Project5/Project5/Project5.cpp
+++ 3451/project 5/Project5/Project5/Project5.cpp
@@ -2,14 +2,30 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
 #include <glut.h>
-//#include <fstream>
+using namespace std;
 
 #define WWIDTH 600
 #define WHEIGHT 600
 
 // prototypes
 void printUsage(void);
+
+// structures
+struct {
+	int vertexCount;
+	int faceCount;
+	float **vertexes[3]; // pointer to a double array of vertexes
+	int **faces[3]; // pointer to a double array of faces
+} model;
+
+// runtime variables
+char* filename;
+int subdivisions;
 
 // Display function
 void display(void){
@@ -26,8 +42,20 @@ void display(void){
 // loads model from file if the syntax is correct. If not correct, then prints usage and exits.
 void myLoadModel(int argc, char** argv)
 {
-	if(argc == 2){
-
+	if(argc == 3){
+		filename = argv[1];
+		subdivisions = atoi(argv[2]);
+		model.vertexCount = 5;
+		ifstream file;
+		file.open(filename);
+		if(file.is_open()){
+			// assumptions: first line vertex, second line faces. always 3 vertices per face.
+			file.close();
+		} else {
+			fprintf(stdout, "Error opening file. Usage:\n");
+			printUsage();
+			exit(0);
+		}
 
 	} else { // incorrect number of args
 		fprintf(stdout, "Incorrect number of arguments (expected 2). Usage:\n");
@@ -74,8 +102,15 @@ void specialKeyboard(int key, int x, int y)
 }
 
 // Program
-int main(int argc, char** argv)
+int main(int argc, char *argv[])
 {
+	// input hook
+	argv = new char*[3];
+	argv[0] = "Project5.exe";
+	argv[1] = "icos.ply";
+	argv[2] = "5";
+	argc = 3;
+
 	// glutinit modifies argc and argv, extracting any args intended for glut.
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
@@ -95,5 +130,5 @@ int main(int argc, char** argv)
 
 void printUsage(void){
 	fprintf(stdout, "Project5.exe [model] [subdivide#]\n");
-	fprintf(stdout, "e.g. Project5.exe octa.ply 5");
+	fprintf(stdout, "e.g. Project5.exe octa.ply 5\n");
 }
